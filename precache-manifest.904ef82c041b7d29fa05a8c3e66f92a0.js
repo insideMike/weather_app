c2a7bcd7c3336236e42f39f7d064226a1bd8330f
insideMike <m.wr00blewsky@gmail.com>
2021-08-10 14:53:08 +0200

    Updates

---
+++ precache-manifest.904ef82c041b7d29fa05a8c3e66f92a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "694ba373b7aa2f21ae7b",
+    "url": "/weather_app/static/css/main.7c503734.chunk.css"
+  },
+  {
+    "revision": "694ba373b7aa2f21ae7b",
+    "url": "/weather_app/static/js/main.947cd73e.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/weather_app/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "05ac5342418cd7c416ad537debb7e2e2",
+    "url": "/weather_app/index.html"
+  }
+];
